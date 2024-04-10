65fe86957edbe5a20f382ee1d22dd521a7df3941
tusya11 <seredanatali250201@gmail.com>
2024-04-11 01:07:34 +0300

    zxc

--- src/dolyami/dolyami.controller.ts
+++ src/dolyami/dolyami.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { DolyamiService } from './dolyami.service';
 import { OrderDTO } from 'src/dto/order.dto';
 
@@ -7,7 +7,8 @@ export class DolyamiController {
   constructor(private readonly dolyamiService: DolyamiService) {}
 
   @Post('create')
-  async create(@Body() data: OrderDTO) {
+  async create(@Body() data: OrderDTO, @Res() res) {
+    res.send(data);
     return await this.dolyamiService.createOrder(data);
   }
 }
