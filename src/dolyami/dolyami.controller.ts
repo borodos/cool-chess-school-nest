af6057c695b2be43d9d2494908de77af52067d38
tusya11 <seredanatali250201@gmail.com>
2024-04-11 01:03:57 +0300

    1231

--- src/dolyami/dolyami.controller.ts
+++ src/dolyami/dolyami.controller.ts
@@ -7,7 +7,7 @@ export class DolyamiController {
   constructor(private readonly dolyamiService: DolyamiService) {}
 
   @Post('create')
-  create(@Body() data: OrderDTO) {
+  async create(@Body() data: OrderDTO) {
     return this.dolyamiService.createOrder(data);
   }
 }
