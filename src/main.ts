78dc3656d484f8fb34e7b1aba75ad000f51989f5
tusya11 <seredanatali250201@gmail.com>
2024-04-11 00:47:31 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpService } from '@nestjs/axios';
-import { InternalServerErrorException } from '@nestjs/common';
 
 async function bootstrap() {
-  const httpService = new HttpService();
   const app = await NestFactory.create(AppModule);
-
-  httpService.axiosRef.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-      console.error('Internal server error exception', error);
-      throw new InternalServerErrorException();
-    },
-  );
 
   app.enableCors({
     allowedHeaders: '*',
     origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
 
   await app.listen(3000);
