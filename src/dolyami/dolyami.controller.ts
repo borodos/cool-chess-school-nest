fcfd2dfe88a3f9a0cc5a1a31e31f3d7f740b402f
tusya11 <seredanatali250201@gmail.com>
2024-04-11 13:59:40 +0300

    change request

--- src/dolyami/dolyami.controller.ts
+++ src/dolyami/dolyami.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, Post, Res } from '@nestjs/common';
 import { DolyamiService } from './dolyami.service';
 import { OrderDTO } from 'src/dto/order.dto';
+import axios from 'axios';
 
 @Controller('dolyami')
 export class DolyamiController {
@@ -8,7 +9,7 @@ export class DolyamiController {
 
   @Post('create')
   @HttpCode(200)
-  async create(@Body() data: OrderDTO): Promise<any> {
+  async create(@Body() data: OrderDTO) {
     return this.dolyamiService.createOrder(data);
   }
 }
